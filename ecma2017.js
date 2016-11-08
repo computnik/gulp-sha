9c999d1c781d972a1e5b942507e4370bc2c2444c
Nikhil Motiani <motianinikhil@gmail.com>
2016-11-08 16:11:49 +0530

    Documentation of Parameters

--- ecma2017.js
+++ ecma2017.js
@@ -3,7 +3,7 @@ let through2 = require('through2');
 let jsSHA = require('jssha');
 
 module.exports =
-  ({hashType = "SHA-256", encoding = "UTF8", inputType = "TEXT", format = "HEX", numRounds = 1, outputUpper = false, b64Pad = "=",  useHmac = false, HMACKey = "", HMACType = "TEXT", shakeLen = 0}) => {
+  ({hashType = "SHA-256", encoding = "UTF8", inputType = "TEXT", outputType = "HEX", numRounds = 1, outputUpper = false, b64Pad = "=", useHmac = false, HMACKey = "", HMACType = "TEXT", shakeLen = 0}) => {
     let CWD = process.cwd();
     return through2.obj(function (file, enc, callback) {
       if (file.isNull()) {
@@ -15,10 +15,10 @@ module.exports =
       if (useHmac) {
         shaObj.setHMACKey(HMACKey, HMACType, {encoding, numRounds});
         shaObj.update(content);
-        result = shaObj.getHMAC(format,{outputUpper, b64Pad, shakeLen})
+        result = shaObj.getHMAC(outputType, {outputUpper, b64Pad, shakeLen})
       } else {
         shaObj.update(content);
-        result = shaObj.getHash(format,{outputUpper, b64Pad, shakeLen})
+        result = shaObj.getHash(outputType, {outputUpper, b64Pad, shakeLen})
       }
       file.contents = new Buffer(result);
       callback(null, file);
